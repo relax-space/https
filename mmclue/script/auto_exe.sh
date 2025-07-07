d6d54f85c8a3bfaff5fedf13285eaceea7c6de29
xiaoxinmiao <xiaoxm_168@163.com>
2025-07-07 21:40:37 +0800

    fix auto_exe

--- mmclue/script/auto_exe.sh
+++ mmclue/script/auto_exe.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-0 2 * * * /home/xxm/dockerpath/https/mmclue/script/mm_nginx_update.sh >> /home/xxm/dockerpath/https/mmclue/script/logs/auto_update.log 2>&1
+/home/xxm/dockerpath/https/mmclue/script/mm_nginx_update.sh >> /home/xxm/dockerpath/https/mmclue/script/logs/auto_update.log 2>&1
